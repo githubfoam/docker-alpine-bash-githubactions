2187166a368e6f2d7b255c3bf84c40942cedd24e
Your Name <you@example.com>
2022-03-07 10:10:30 +0300

    busybox diff

--- dockerfiles/updater_cvs.sh
+++ dockerfiles/updater_cvs.sh
@@ -55,7 +55,7 @@ do
     wget --quiet --no-check-certificate ${file}
     filename=$(echo ${file} | awk -F/ '{print $NF}')
     # result=$(diff --suppress-common-lines --speed-large-files -y ${filename} ../../../${filename} | wc -l)
-    result=$(diff --suppress-common-lines --speed-large-files -y ${filename} ../${filename} | wc -l)    
+    result=$(diff ${filename} ../${filename} | wc -l)    
     
     if [ ${result} -ne 0 ]; then
 	logIfDebug "Updating ${filename} as it differs"
